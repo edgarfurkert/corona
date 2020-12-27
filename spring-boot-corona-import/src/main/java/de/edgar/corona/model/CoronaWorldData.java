24167b8a0a5ec3f078d6a22e55e80273d82c68e0
Edgar Furkert <edgar.furkert@t-online.de>
2020-12-27 16:49:32 +0100

    Import: support new corona world data source

--- spring-boot-corona-import/src/main/java/de/edgar/corona/model/CoronaWorldData.java
+++ spring-boot-corona-import/src/main/java/de/edgar/corona/model/CoronaWorldData.java
@@ -1,14 +1,77 @@
 package de.edgar.corona.model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CoronaWorldData extends CoronaData {
 
 	public CoronaWorldData() {
 	}
 	
+	/**
+	 * https://covid.ourworldindata.org/data/owid-covid-data.csv
+	 * 
+	 *  0. iso_code,
+	 *  1. continent,
+	 *  2. location,
+	 *  3. date,
+	 *  4. total_cases,
+	 *  5. new_cases,
+	 *  6. new_cases_smoothed,
+	 *  7. total_deaths,
+	 *  8. new_deaths,
+	 *  9. new_deaths_smoothed,
+	 * 10. total_cases_per_million,
+	 * 11. new_cases_per_million,
+	 * 12. new_cases_smoothed_per_million,
+	 * 13. total_deaths_per_million,
+	 * 14. new_deaths_per_million,
+	 * 15. new_deaths_smoothed_per_million,
+	 * 16. reproduction_rate,
+	 * 17. icu_patients,
+	 * 18. icu_patients_per_million,
+	 * 19. hosp_patients,
+	 * 20. hosp_patients_per_million,
+	 * 21. weekly_icu_admissions,
+	 * 22. weekly_icu_admissions_per_million,
+	 * 23. weekly_hosp_admissions,
+	 * 24. weekly_hosp_admissions_per_million,
+	 * 25. new_tests,
+	 * 26. total_tests,
+	 * 27. total_tests_per_thousand,
+	 * 28. new_tests_per_thousand,
+	 * 29. new_tests_smoothed,
+	 * 30. new_tests_smoothed_per_thousand,
+	 * 31. positive_rate,
+	 * 32. tests_per_case,
+	 * 33. tests_units,
+	 * 34. total_vaccinations,
+	 * 35. total_vaccinations_per_hundred,
+	 * 36. stringency_index,
+	 * 37. population,
+	 * 38. population_density,
+	 * 39. median_age,
+	 * 40. aged_65_older,
+	 * 41. aged_70_older,
+	 * 42. gdp_per_capita,
+	 * 43. extreme_poverty,
+	 * 44. cardiovasc_death_rate,
+	 * 45. diabetes_prevalence,
+	 * 46. female_smokers,
+	 * 47. male_smokers,
+	 * 48. handwashing_facilities,
+	 * 49. hospital_beds_per_thousand,
+	 * 50. life_expectancy,
+	 * 51. human_development_index
+	 * 
+	 * @param line
+	 */
 	public CoronaWorldData(String line) {
 		String[] a = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)");
+		/*
 		setDay(getInteger(a[1]));
 		setMonth(getInteger(a[2]));
 		setYear(getInteger(a[3]));
@@ -21,6 +84,28 @@ public class CoronaWorldData extends CoronaData {
 		setTerritoryCode(a[8].length() > 0 ? a[8] : a[7]);
 		setPopulation(getLong(a[9]));
 		setTerritoryParent(getKey(a[10]));
+		*/
+		if (a.length < 38) {
+			log.debug("Length: {} {}", a.length, line);
+			setDateRep(LocalDate.now().plusDays(1)); // filter
+			return;
+		}
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate date = LocalDate.parse((a[3]), formatter);
+		setDateRep(date);
+		setDay(date.getDayOfMonth());
+		setMonth(date.getMonthValue());
+		setYear(date.getYear());
+		setCases(getLong(a[5]));
+		setCasesKum(getLong(a[4]));
+		setDeaths(getLong(a[8]));
+		setDeathsKum(getLong(a[7]));
+		setTerritory(a[2].replaceAll("\"", ""));
+		setTerritoryId(getKey(getTerritory()));
+		setGeoId(a[0]);
+		setTerritoryCode(a[0]);
+		setPopulation(getLong(a[37]));
+		setTerritoryParent(getKey(a[1]));
 		setOrderId(OrderIdEnum.COUNTRY.getOrderId());
 	}
 
